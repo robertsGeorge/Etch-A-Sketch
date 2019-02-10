313a45a9c8df7dbb9354a14ef20a68a12efb8e83
George Roberts <george.e.roberts@gmail.com>
2019-02-10 12:20:44 +0000

    generate div grid container with 16 child divs

---
+++ script.js
@@ -0,0 +1,7 @@
+let gridContainer = document.querySelector(".grid-container");
+
+
+for (let i = 1; i <= 16; i++) {
+  gridSquare = document.createElement("div");
+  gridContainer.appendChild(gridSquare);
+}
