29bec9630e327270380f1d3974b06686a5304749
George Roberts <george.e.roberts@gmail.com>
2019-02-15 12:27:11 +0000

    add flexbox-based grid, and mouseover event create a for loop that adds an event listener to each grid square within allGridSquares NodeList, then runs function changeColor.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let gridContainer = document.querySelector(".grid-container");
+let gridSquare;
 
-
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
   gridSquare = document.createElement("div");
   gridContainer.appendChild(gridSquare);
 }
+
+allGridSquares = document.querySelectorAll(".grid-container div");
+
+for (let i = 0; i < allGridSquares.length; i++) {
+  allGridSquares[i].addEventListener("mouseover", changeColor);
+}
+
+function changeColor() {
+  this.style.backgroundColor = "black";
+}
+
