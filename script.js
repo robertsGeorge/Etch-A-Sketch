b8f1ca7f9bf3be29e91584ae2c37cbd5bc4cb93a
George Roberts <george.e.roberts@gmail.com>
2019-02-16 12:51:10 +0000

    add revertColor function and general comments

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 function changeColor() {
   this.style.backgroundColor = "black";
 }
+function revertColor() {
+  for (let i = 0; i < allGridSquares.length; i++) {
+    allGridSquares[i].style.backgroundColor = "white";
+  }
+}
 
+let buttonClear = document.getElementById("clear");
 let gridContainer = document.querySelector(".grid-container");
 let gridSquare;
 
+// generate grid within container
 for (let i = 1; i <= 256; i++) {
   gridSquare = document.createElement("div");
   gridContainer.appendChild(gridSquare);
 }
 
+// change gridsquare color when mouse hovers over
 let allGridSquares = document.querySelectorAll(".grid-container div");
-
 for (let i = 0; i < allGridSquares.length; i++) {
   allGridSquares[i].addEventListener("mouseover", changeColor);
 }
 
-let clearButton = document.getElementById("clear")
+// change color of grid squares back to white
+buttonClear.addEventListener("click", revertColor);
 
